eaadc0f1f062b6b61e71ba0efcf47d1fb8919f39
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-07-08 09:10:26 +0200

    base

---
+++ src/main/java/com/generation/e_school/ESchoolApplication.java
@@ -0,0 +1,13 @@
+package com.generation.e_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/e_school/ESchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.e_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
